230c7a318ac5131e574272115eddf4983db3db3a
Martine <maskor_@outlook.com>
2024-05-07 11:44:24 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,25 +6,25 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Martine - UI Designer & Frontend Developer</title>
-        <meta name="title" content="Martine - UI Designer & Frontend Developer"/>
+        <title>Martine - Front-end Developer & UI Designer</title>
+        <meta name="title" content=" Martine - Frontend Developer & UI Designer"/>
         <meta name="description" content="https://portfolio-marskor.vercel.app/"/>
 
         {/* <!-- Primary Meta Tags --> */}
-        <meta name="title" content="Martine - UI Designer & Frontend Developer"/>
+        <meta name="title" content=" Martine - Front-end Developer & UI Designer"/>
         <meta name="description" content=""/>
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="https://metatags.io/"/>
-        <meta property="og:title" content="Martine - UI Designer & Frontend Developer"/>
+        <meta property="og:title" content="Martine - Front-end Developer & UI Designer"/>
         <meta property="og:description" content=""/>
         <meta property="og:image" content="/assets/backgrounds/under-construction.png"/>
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://metatags.io/"/>
-        <meta property="twitter:title" content="Martine - UI Designer & Frontend Developer"/>
+        <meta property="twitter:title" content="Martine - Front-end Developer & UI Designer"/>
         <meta property="twitter:description" content=""/>
         <meta property="twitter:image" content="/assets/backgrounds/under-construction.png"/>
 
@@ -39,15 +39,15 @@ export default function Home() {
 
       <main className='wrapper content'>
         <div className="content__container">
-          <h2>Martine - Developer & Designer</h2>
+          <h2>Martine - Front-end Developer & UI Designer</h2>
           <h1>Website Under Construction</h1>
           <div className="content__links">
-            <a href="mailto:marty99skor@gmail.com"><Image src="/assets/icons/mail.svg" height={20} width={20} alt='mail'/> Email</a>
+            <a href="mailto:martinesskor@gmail.com"><Image src="/assets/icons/mail.svg" height={20} width={20} alt='mail'/> Email</a>
             <a target='_blank' href="https://www.behance.net/maskor_"> <Image src="/assets/icons/behance.svg" height={20} width={20} alt='behance'/> Behance</a>
             <a target='_blank' href="https://bento.me/martine"><Image src="/assets/icons/link.svg" height={20} width={20} alt='link'/> Bento</a>
           </div>
           <Image className='tech-img' src="/assets/tech-img.svg" width={300} height={300} alt='tech'/>
-          <p className='portfolio-blur'>Portfolio 2023</p>
+          <p className='portfolio-blur'>Portfolio 2024</p>
         </div>
       </main>
     </>
